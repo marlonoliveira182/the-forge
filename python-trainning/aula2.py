50f7996a3180befbabb635feb7af2621bb57d4f2
Marlon Vinicius de Oliveira <marlonvo@gmail.com>
2025-04-06 17:11:02 -0300

    novas aulas

---
+++ python-trainning/aula2.py
@@ -0,0 +1,7 @@
+# This is a simple example of a Python script that creates a list and appends numbers to it.
+data_model = []
+
+for i in range(10):
+    data_model.append(i)
+    print(data_model[i])
+    
