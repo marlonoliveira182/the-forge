d0b76e118e529442edf6ee63a233490b261548ea
Marlon Oliveira <marlonvo@gmail.com>
2025-06-15 10:35:43 -0300

    For examples

--- python-trainning/while-for.py
+++ python-trainning/while-for.py
@@ -8,6 +8,8 @@
 #     count *= 2
 #     print(count)
     
+#########################################################################
+#########################################################################
 
 #Print all values from 1 to 100. If count = 51, then exit For.
 # for count in range(1, 101):
@@ -16,23 +18,41 @@
 #         break
 #     print(count)
 
+#########################################################################
+#########################################################################
 
 #Print all even numbers from 1 to 100.
 # for count in range(0, 101):
 #     if count % 2 != 0:
 #         continue
 #     print(count)
-      
-#Matrix
-max_rows = 10
-max_cols = 10
-row = 0
-col = 0
 
-while row <= max_rows:
-    col = 0
-    while col <= max_cols:
-        print (f'{row},{col}', end = ' ')
-        col += 1
-    print()
-    row += 1    
+#########################################################################
+#########################################################################   
+   
+# Matrix
+# max_rows = 10
+# max_cols = 10
+# row = 0
+# col = 0
+
+# while row <= max_rows:
+#     col = 0
+#     while col <= max_cols:
+#         print(f'{row},{col}', end=' ')
+#         col += 1
+#     print()
+#     row += 1    
+
+#########################################################################
+#########################################################################
+
+#For
+
+# text = 'Python'
+
+# for letter in text:
+#     print(letter)
+
+
+
